b04236c856bbea4b5a518338e037adeb668eddad
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-06 13:33:13 +0300

    Add a class for pitch categories.

--- app/models.py
+++ app/models.py
@@ -27,3 +27,24 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+class Category(db.Model):
+
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.String(255))
+
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        categories = Category.query.all()
+        return categories
+
+

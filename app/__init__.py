a25450351a6f526ee98cacf8aa21319fe3eb2385
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-06 10:58:31 +0300

    Add a function to initialize app and flask extensions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,24 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    #creating the app configurations
+    app.config.from_object(config_options[config_name])
+    config_options[config_name]
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
+
+    return app

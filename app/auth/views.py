313f39bfc75ffc22e42c65ab73003521a759eaad
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-07 11:06:00 +0300

    Add a register function for new account.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
+from flask_login import login_user,logout_user,login_required
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from ..email import mail_message
 
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to 60 seconds impression","email/welcome_user",user.email,user=user)
+        
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)

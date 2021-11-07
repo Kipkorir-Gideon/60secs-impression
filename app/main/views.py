7b61a73b806bf449c5b397d68a904ff23442afe4
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-07 12:53:34 +0300

    Add a route with a function to add a new category.

--- app/main/views.py
+++ app/main/views.py
@@ -21,7 +21,9 @@ def index():
 @main.route('/category/new-pitch/<int:id>', methods=['GET', 'POST'])
 @login_required
 def pitch(id):
-    ''' Function to check Pitches form and fetch data from the fields '''
+    ''' 
+    Function to check Pitches form and fetch data from the fields
+    '''
     form = PitchForm()
     category = Category.query.filter_by(id=id).first()
 
@@ -50,3 +52,22 @@ def category(id):
 
     pitches=Pitch.get_pitches(id)
     return render_template('category.html', pitches=pitches, category=category)
+
+
+@main.route('/add/category', methods=['GET','POST'])
+@login_required
+def new_category():
+    '''
+    View new group route function that returns a page with a form to create a category
+    '''
+    form = CategoryForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = Category(name=name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
+    title = 'New category'
+    return render_template('new_category.html', category_form = form,title=title)

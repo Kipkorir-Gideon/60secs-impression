9028ec57b893c7306d7167819108ebc41701a3d3
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-07 11:29:39 +0300

    Add an index function on views to display categories on landing page.

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,17 @@
-from flask_login import login_required
+from flask import render_template, request, redirect, url_for, abort
+from . import main
+from ..models import User,Pitch,Comments,Category,Votes
+from .. import db
+from . forms import PitchForm, CommentsForm, CategoryForm
+from flask_login import login_required,current_user
+
+
+#display categories on the landing page
+@main.route('/')
+def index():
+    """ View root page function that returns index page """
+
+    category = Category.get_categories()
+
+    title = 'Home- Welcome'
+    return render_template('index.html', title = title, categories=category)

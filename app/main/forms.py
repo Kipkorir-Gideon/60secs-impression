02e60dbeff4a853a7b131259d1691b8bdd0ab3ed
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-11-07 11:20:03 +0300

    Create a class for pitching form.

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+
+class PitchForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a pitch
+    '''
+    content = TextAreaField('Impress')
+    submit = SubmitField('Submit')
